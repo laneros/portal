f56b5fc47b2c93c06eba7141453699f9aac198ee
Julian Muñoz <juliand@laneros.com>
2019-03-18 00:34:49 -0500

    Eliminamos el bbcode extra que no necesitamos, adicional utilizamos un on document ready aunque no está siendo eficaz cuando una de las imágenes no alcanza a cargar.

--- Laneros/Portal/Pub/View/Portal/View.php
+++ Laneros/Portal/Pub/View/Portal/View.php
@@ -2,11 +2,15 @@
 
 namespace Laneros\Portal\Pub\View\Portal;
 
+use XF\Str\Formatter;
+
 class View extends \XF\Mvc\View
 {
     public function renderHtml()
     {
         $router = \XF::app()->router('public');
+
+        $formatter = new Formatter();
 
         /** @var \Laneros\Portal\Repository\FeaturedThread $thread */
         foreach($this->params['featuredThreads'] AS $key => &$thread)
@@ -17,6 +21,9 @@ class View extends \XF\Mvc\View
             preg_match('/\[(img|IMG)\]\s*(https?:\/\/([^*\r\n]+|[a-z0-9\/\\\._\- !]+))\[\/(img|IMG)\]/', $message, $matches);
 
             $this->params['portalImages'][$key] = isset($matches[2]) ? $matches[2] : '';
+
+            // Limpiamos el mensaje de bbcodes / imagenes / espacios
+            $thread->Thread->FirstPost->message = $formatter->stripBbCode($message, ['stripQuote' => true, ]);
 
             if (empty($thread['portalImages']))
             {
