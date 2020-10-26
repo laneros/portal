21473692edc3afbaf4eda1d7365c0fdedb2f710b
Julian Munoz <juliand@laneros.com>
2020-10-26 17:51:55 -0500

    Agregamos opción de snippet, agregamos el autor de la noticia.

--- Pub/View/Portal/View.php
+++ Pub/View/Portal/View.php
@@ -13,17 +13,20 @@ class View extends \XF\Mvc\View
 
         $router = \XF::app()->router('public');
 
+        $snippet = $app->options()->lanerosPortalSnippet;
+
         $formatter = new Formatter();
 
         /** @var \Laneros\Portal\Repository\FeaturedThread $featuredThread */
-        foreach($this->params['featuredThreads'] AS $key => &$featuredThread)
+        $featuredThreads = $this->getParams();
+        foreach ($this->params['featuredThreads'] as $postId => &$featuredThread)
         {
             $message = $featuredThread->Thread->FirstPost->message;
 
             // Sacamos la primra imagen del post sea de IMG o ATTACH
             preg_match('/\[(img|IMG)\]\s*(https?:\/\/([^*\r\n]+|[a-z0-9\/\\\._\- !]+))\[\/(img|IMG)\]/', $message, $matches);
 
-            $this->params['portalImages'][$key] = isset($matches[2]) ? $matches[2] : '';
+            $this->params['portalImages'][$postId] = isset($matches[2]) ? $matches[2] : '';
 
             // Limpiamos el mensaje de bbcodes / imagenes / espacios
             $featuredThread->Thread->FirstPost->message = $formatter->stripBbCode($message, ['stripQuote' => true, ]);
@@ -33,7 +36,7 @@ class View extends \XF\Mvc\View
                 preg_match_all('#\[attach[^\]]*\](?P<id>\d+)(\D.*)?\[/attach\]#iU', $message, $matches);
 
                 if (! empty($matches['id'])) {
-                    $this->params['portalImages'][$key] = $router->buildLink('full:attachments', ['attachment_id' => $matches['id'][0]]);
+                    $this->params['portalImages'][$postId] = $router->buildLink('full:attachments', ['attachment_id' => $matches['id'][0]]);
                 }
             }
 
@@ -42,7 +45,11 @@ class View extends \XF\Mvc\View
                 'stripQuote' => true,
             ];
 
-            $featuredThread->Thread->FirstPost->message = $stringFormatter->stripBbCode($message, $stripBbCodeOptions);
+            if ($snippet['enabled'] == true) {
+                $featuredThread->Thread->FirstPost->message = $stringFormatter->stripBbCode($message, $stripBbCodeOptions);
+            } else {
+                $featuredThread->Thread->FirstPost->message = '';
+            }
 
             if (! empty($featuredThread->featured_title))
             {
